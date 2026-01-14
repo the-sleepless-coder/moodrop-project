547faa814a1ccca1d9da585646dd796d3b53c261
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2026-01-14 20:25:45 +0900

    feat:add CORS configuration code

--- spring-app/src/main/java/com/moodrop/config/WebConfig.java
+++ spring-app/src/main/java/com/moodrop/config/WebConfig.java
@@ -13,6 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
-	
-	
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:8081")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
 }
